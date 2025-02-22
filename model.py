622f086a3d2ff700e6d791a94d3bb5d716ae4b68
BloodPS <153712377+BloodPS@users.noreply.github.com>
2025-02-22 13:30:36 +0700

    Update model.py

--- model.py
+++ model.py
@@ -80,8 +80,8 @@ def predict():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0",port=port)
+    port = int(os.environ.get("PORT", 5000))  # ใช้ PORT จาก Environment Variable
+    app.run(host="0.0.0.0", port=port)
 
 
 
