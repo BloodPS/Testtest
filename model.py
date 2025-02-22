d92c1aa3cea9cced7e5b7294bd911105e3f65c6c
BloodPS <153712377+BloodPS@users.noreply.github.com>
2025-02-22 15:25:07 +0700

    Update model.py

--- model.py
+++ model.py
@@ -82,8 +82,7 @@ def predict():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     print(f"Starting the app on port {port}")  # เพิ่มข้อความ log
-    app.run(host="0.0.0.0", port=port)        port = int(os.environ.get("PORT", 5000))  # ใช้ PORT จาก Railway
-        app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
 
 
 
